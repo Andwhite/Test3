e25e333f6b7497f68e41bd18a0f4f63c1bc11603
Andwhite <gritsenyuk@gmail.com>
2015-05-19 00:20:21 +0300

    1st commit

---
+++ Test3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test3
+//
+//  Created by Andrey on 19.05.15.
+//  Copyright (c) 2015 Andwhite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
